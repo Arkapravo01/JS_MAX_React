1967c1109cc9456266468b74ceb55d42cfec6bf3
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-01-06 22:37:19 +0530

    18

---
+++ First/assets/scripts/app.js
@@ -0,0 +1,11 @@
+// import apiKey from "./util.js";
+// import {apiKey as content} from "./util.js";
+// import * as util from "./util.js";
+
+// console.log(content)
+// console.log(util.apiKey);
+// console.log(util.apiKey2);
+
+if(10 === 10){
+    console.log('works')
+}
---
+++ First/assets/scripts/util.js
@@ -0,0 +1,6 @@
+export default "adanfjancoanoialvjjanola";
+
+export let apiKey = "afkaomkamvkampam";
+export let apiKey2 = "afkaomkamvkampam2";
+
+
